cf536119b4a0f23df2b43b0ab051add787237b78
diegogirao <diegoramiro25@gmail.com>
2023-04-06 14:57:21 +0100

    Update rearrange files and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Menu from "./components/Menu"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import Showroom from "./components/Showroom/Showroom"
 import "./index.css"
 
 function App() {
 	return (
-		<div className="container bg-emerald-200 grid mx-auto w-screen desktop:grid-cols-4 tablet:grid-cols-2 tablet:place-items-center mobile:grid-cols-1 mobile:place-items-center">
-			<Menu />
-			<h1 className="text-3xl font-bold underline">Hello Room LP !!!</h1>
+		<div>
+			<Header />
+			<Showroom />
+			<Footer />
 		</div>
 	)
 }

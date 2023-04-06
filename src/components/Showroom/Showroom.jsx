707316255cd724898c9693e0d26043f2ee6a246f
diegogirao <diegoramiro25@gmail.com>
2023-04-06 15:30:13 +0100

    Feat created showroom function

--- src/components/Showroom/Showroom.jsx
+++ src/components/Showroom/Showroom.jsx
@@ -1,5 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
+
+const data = [
+	{
+		id: 1,
+		title: "Discover innovative ways to decorate",
+		desc: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+		mobile: "./images/mobile-image-hero-1.jpg",
+		desktop: "./images/desktop-image-hero-1.jpg",
+	},
+]
 
 export default function Showroom() {
-	return <div>Showroom</div>
+	const [itens] = useState(data)
+
+	return (
+		<>
+			<h1>Showroom</h1>
+		</>
+	)
 }

c29626b9fa26baa144861d6bd256efb6bdb4e4c1
diegogirao <diegoramiro25@gmail.com>
2023-04-06 18:01:57 +0100

    Update logo and hamburger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,22 +4,30 @@ import menu from "/src/assets/images/icon-hamburger.svg"
 import close from "/src/assets/images/icon-close.svg"
 
 export default function Header() {
-	const [isOpen, setIsOpen] = useState(false)
+	// const [isOpen, setIsOpen] = useState(false)
 
 	return (
 		<>
-			<header>
+			<header className="absolute z-10 p-8 w-full flex items-start justify-start">
 				<div>
-					<img src={logo} alt="room logo" className="lg:mr-8" />
+					<img src={logo} alt="room logo" />
 				</div>
 
-				<div
+				<nav></nav>
+
+				<div>
+					<button>
+						<img src={menu} alt="hamburger menu" />
+					</button>
+				</div>
+
+				{/* <div
 					className={`${
 						isOpen
 							? "bg-black bg-opacity-75 absolute top-0 left-0 right-0 bottom-0 h-screen"
 							: "bg-transparent lg:bg-transparent"
 					} lg:bg-transparent lg:h-auto lg:relative`}
-				></div>
+				></div> */}
 			</header>
 		</>
 	)

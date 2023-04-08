c10a7eb64f6231cf6c2e0af8b585739828a1f917
diegogirao <diegoramiro25@gmail.com>
2023-04-08 14:40:25 +0100

    Style fix classname position, line 20

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
 						isOpen
 							? "bg-black bg-opacity-75 absolute top-0 left-0 right-0 bottom-0 h-screen"
 							: "bg-transparent lg:bg-transparent"
-					}lg:bg-transparent lg:h-auto lg:relative`}
+					} lg:bg-transparent lg:h-auto lg:relative `}
 				>
 					<nav
 						className={`${isOpen ? "open w-full py-8 lg:py-0 lg:w-auto" : ""}`}
